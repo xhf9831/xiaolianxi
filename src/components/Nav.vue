99adb77d2a1f7b9472b00dff35e8e9c61c96b22e
xhf9831 <18215615808@163.com>
2019-10-17 18:28:17 +0800

    我做好了

---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Nav
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {},
+  created() {},
+  filters: {},
+  computed: {},
+  watch: {},
+  directives: {}
+};
+</script>
+
+<style scoped lang="scss"></style>
